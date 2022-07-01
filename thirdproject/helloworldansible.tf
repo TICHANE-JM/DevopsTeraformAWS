3292e03dcd3ea5fdc1a8e89cdc47511d37e8cbeb
TICHANE Jean-Michel <107214400+TICHANE-JM@users.noreply.github.com>
2022-07-01 17:15:16 +0200

    Create helloworldansible.tf

---
+++ thirdproject/helloworldansible.tf
@@ -0,0 +1,37 @@
+# Configuration du fournisseur pour AWS
+provider "aws" {
+  region     = "us-east-1"
+}
+
+# Configuration des ressources pour AWS
+resource "aws_instance" "myserver" {
+  ami = "ami-cfe4b2b0"
+  instance_type = "t2.micro"
+  key_name = "EffectiveDevOpsAWS"
+  vpc_security_group_ids = ["sg-01864b4c"]
+
+  tags {
+    Name = "helloworld"
+  }
+
+# Approvisionnement pour l'application du playbook Ansible
+  provisioner "remote-exec" {
+    connection {
+      user = "ec2-user"
+      private_key = "${file("/root/.ssh/aws-devops.pem")}"
+    }
+  }
+  
+  provisioner "local-exec" {
+    command = "sudo echo '${self.public_ip}' > ./myinventory",
+  }
+
+  provisioner "local-exec" {
+    command = "sudo ansible-playbook -i myinventory --private-key=/root/.ssh/aws-devops.pem helloworld.yml",
+  }  
+}
+
+# Adresse IP de l'instance EC2 nouvellement créée
+output "myserver" {
+ value = "${aws_instance.myserver.public_ip}"
+}
